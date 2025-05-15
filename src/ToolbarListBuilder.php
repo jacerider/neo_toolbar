7e8361608216b9117465bf21c782a49676cb4800
JaceRider <jacerider@gmail.com>
2025-05-15 10:48:17 -0500

    ✨ (ToolbarListBuilder.php): introduce IconTranslationTrait to enhance status display with icons in toolbar listing

--- src/ToolbarListBuilder.php
+++ src/ToolbarListBuilder.php
@@ -7,11 +7,14 @@ namespace Drupal\neo_toolbar;
 use Drupal\Core\Config\Entity\DraggableListBuilder;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Url;
+use Drupal\neo_icon\IconTranslationTrait;
 
 /**
  * Provides a listing of toolbars.
  */
 final class ToolbarListBuilder extends DraggableListBuilder {
+
+  use IconTranslationTrait;
 
   /**
    * {@inheritdoc}
@@ -37,7 +40,7 @@ final class ToolbarListBuilder extends DraggableListBuilder {
     /** @var \Drupal\neo_toolbar\ToolbarInterface $entity */
     $row['label'] = $entity->label();
     $row['id']['data']['#markup'] = $entity->id();
-    $row['status']['data']['#markup'] = $entity->status() ? $this->t('Enabled') : $this->t('Disabled');
+    $row['status']['data']['#markup'] = $this->statusIcon($entity->status())->iconOnly();
     return $row + parent::buildRow($entity);
   }
 
